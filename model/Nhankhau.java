b3c88b7d512035fae44484502ff196b8164a5998
TranAnhVu0411 <72057891+TranAnhVu0411@users.noreply.github.com>
2020-10-19 20:20:42 +0700

    Add files via upload

---
+++ model/Nhankhau.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cnpm.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Vu
+ */
+public class Nhankhau {
+    private String hoten;
+    private String bidanh;
+    private Date ngaysinh;
+    private String noisinh;
+    private String nguyenquan;
+    private String dantoc;
+    private String nghenghiep;
+    private String noilamviec;
+    private String cmnd;
+    private Date ngaycap;
+    private String noicap;
+    private String quanhe;
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+
+    public String getBidanh() {
+        return bidanh;
+    }
+
+    public void setBidanh(String bidanh) {
+        this.bidanh = bidanh;
+    }
+
+    public Date getNgaysinh() {
+        return ngaysinh;
+    }
+
+    public void setNgaysinh(Date ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    public String getNoisinh() {
+        return noisinh;
+    }
+
+    public void setNoisinh(String noisinh) {
+        this.noisinh = noisinh;
+    }
+
+    public String getNguyenquan() {
+        return nguyenquan;
+    }
+
+    public void setNguyenquan(String nguyenquan) {
+        this.nguyenquan = nguyenquan;
+    }
+
+    public String getDantoc() {
+        return dantoc;
+    }
+
+    public void setDantoc(String dantoc) {
+        this.dantoc = dantoc;
+    }
+
+    public String getNghenghiep() {
+        return nghenghiep;
+    }
+
+    public void setNghenghiep(String nghenghiep) {
+        this.nghenghiep = nghenghiep;
+    }
+
+    public String getNoilamviec() {
+        return noilamviec;
+    }
+
+    public void setNoilamviec(String noilamviec) {
+        this.noilamviec = noilamviec;
+    }
+
+    public String getCmnd() {
+        return cmnd;
+    }
+
+    public void setCmnd(String cmnd) {
+        this.cmnd = cmnd;
+    }
+
+    public Date getNgaycap() {
+        return ngaycap;
+    }
+
+    public void setNgaycap(Date ngaycap) {
+        this.ngaycap = ngaycap;
+    }
+
+    public String getNoicap() {
+        return noicap;
+    }
+
+    public void setNoicap(String noicap) {
+        this.noicap = noicap;
+    }
+
+    public String getQuanhe() {
+        return quanhe;
+    }
+
+    public void setQuanhe(String quanhe) {
+        this.quanhe = quanhe;
+    }
+    
+}
